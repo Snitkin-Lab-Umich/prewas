7ac298950e67b1dddfc4767de0b6ce705a50582b
Zena Lapp <zenalapp@umich.edu>
2019-10-30 17:00:13 -0400

    add input checks

--- R/generate_allele_matrix.R
+++ R/generate_allele_matrix.R
@@ -1,4 +1,5 @@
 replace_non_ATGC_with_N <- function(mat){
+  check_is_this_class(mat, "mat")
   mat <- apply(mat, 2, toupper)
   mat[is.na(mat)] <- "N"
   mat[!(mat %in% c("A", "T", "G", "C"))] <- "N"
@@ -6,6 +7,7 @@ replace_non_ATGC_with_N <- function(mat){
 }
 
 identify_variant_sites <- function(mat){
+  check_is_this_class(mat, "mat")
   rows_to_keep <- apply(mat, 1, function(row){
     sum(unique(row) %in% c("A", "T", "G", "C"))
   })
@@ -15,11 +17,14 @@ identify_variant_sites <- function(mat){
 }
 
 remove_invariant_sites <- function(mat, rows_to_keep){
+  check_is_this_class(mat, "mat")
+  check_is_this_class(rows_to_keep, "vector")
   mat <- mat[rows_to_keep, , drop = FALSE]
   return(mat)
 }
 
 keep_only_variant_sites <- function(dna_mat){
+  check_is_this_class(dna_mat, "mat")
   dna_mat <- replace_non_ATGC_with_N(dna_mat)
   variant_site_log <- identify_variant_sites(dna_mat)
   variant_only_dna_mat <- remove_invariant_sites(dna_mat, variant_site_log)
