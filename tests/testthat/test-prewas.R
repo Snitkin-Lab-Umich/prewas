4d5307029879cc16dd0b88532a85da1e7d2ccb98
Katie Saund <katiephd@umich.edu>
2019-12-31 10:39:09 -0500

    Update dna input to vcf input

--- tests/testthat/test-prewas.R
+++ tests/testthat/test-prewas.R
@@ -1,6 +1,6 @@
 # prewas ----------------------------------------------------------------------#
 test_that("Check that prewas() gives expected output when given valid input", {
-  test_results <- prewas::prewas(dna = prewas::dna,
+  test_results <- prewas::prewas(dna = prewas::vcf,
                                  tree = prewas::tree,
                                  outgroup = prewas::outgroup,
                                  gff = prewas::gff,
@@ -16,33 +16,33 @@ test_that("Check that prewas() gives an error when given incorrect VCF data", {
 })
 
 test_that("Check that prewas() gives an error when given incorrect tree data", {
-  expect_error(prewas::prewas(dna = prewas::dna, tree = 5))
-  expect_error(prewas::prewas(dna = prewas::dna, tree = "foo"))
-  expect_error(prewas::prewas(dna = prewas::dna, tree = matrix(0, 10, 10)))
+  expect_error(prewas::prewas(dna = prewas::vcf, tree = 5))
+  expect_error(prewas::prewas(dna = prewas::vcf, tree = "foo"))
+  expect_error(prewas::prewas(dna = prewas::vcf, tree = matrix(0, 10, 10)))
 })
 
 test_that("Check that prewas() gives an error when given incorrect outgroup data", {
-  expect_error(prewas::prewas(dna = prewas::dna,
+  expect_error(prewas::prewas(dna = prewas::vcf,
                               tree = prewas:tree,
                               outgroup = 5))
-  expect_error(prewas::prewas(dna = prewas::dna,
+  expect_error(prewas::prewas(dna = prewas::vcf,
                               tree = prewas:tree,
                               outgroup = "foo"))
-  expect_error(prewas::prewas(dna = prewas::dna,
+  expect_error(prewas::prewas(dna = prewas::vcf,
                               tree = prewas:tree,
                               outgroup =  matrix(0, 10, 10)))
 })
 
 test_that("Check that prewas() gives an error when given incorrect gff data", {
-  expect_error(prewas::prewas(dna = prewas::dna,
+  expect_error(prewas::prewas(dna = prewas::vcf,
                               tree = prewas:tree,
                               outgroup = prewas:outgroup,
                               gff = 5))
-  expect_error(prewas::prewas(dna = prewas::dna,
+  expect_error(prewas::prewas(dna = prewas::vcf,
                               tree = prewas:tree,
                               outgroup = prewas:outgroup,
                               gff = "foo"))
-  expect_error(prewas::prewas(dna = prewas::dna,
+  expect_error(prewas::prewas(dna = prewas::vcf,
                               tree = prewas:tree,
                               outgroup = prewas:outgroup,
                               gff = matrix(0, 10, 10)))
