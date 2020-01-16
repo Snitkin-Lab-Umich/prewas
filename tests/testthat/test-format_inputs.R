a8a68256a4c9b7927c419453502012623135c62c
Katie Saund <katiephd@umich.edu>
2020-01-16 14:14:08 -0500

    Update unit test

--- tests/testthat/test-format_inputs.R
+++ tests/testthat/test-format_inputs.R
@@ -1,10 +1,21 @@
 # format_inputs ---------------------------------------------------------------#
 # format_inputs ---------------------------------------------------------------#
-test_that("format_inputs gives no errors when reading in test data included with prewas", {
-  expect_silent(format_inputs(dna = prewas::vcf,
-                              tree = prewas::tree,
-                              outgroup = prewas::outgroup,
-                              gff = prewas::gff))
+test_that("format_inputs gives expected results prewas test data", {
+  vcf <- prewas::vcf
+  tree <- prewas::tree
+  outgroup <- prewas::outgroup
+  gff <- prewas::gff
+  results <- format_inputs(dna = vcf,
+                           tree = tree,
+                           outgroup = outgroup,
+                           gff = gff)
+  expect_equal(length(results), 4)
+  expect_true(methods::is(results$dna, "matrix"))
+  expect_true(methods::is(results$tree, "phylo"))
+  expect_true(methods::is(results$outgroup, "character"))
+  expect_true(methods::is(results$gff, "matrix"))
+
+
 })
 
 test_that("format_inputs gives errors when given no data", {
