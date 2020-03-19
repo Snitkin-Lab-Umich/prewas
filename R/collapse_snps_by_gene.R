2afc39fdbfc409a711cae619c0984c9e17f99f5b
sthiede <thiede@umich.edu>
2020-03-18 23:05:28 -0400

    added two new functions collapse_snps_into_genes_by_impact and get_gene_mat_by_impact to return a list of 5 gene mats based on impact

--- R/collapse_snps_by_gene.R
+++ R/collapse_snps_by_gene.R
@@ -54,3 +54,71 @@ collapse_snps_into_genes <- function(bin_mat, gene_vec){
 
   return(gene_mat)
 }
+
+#' Title
+#'
+#' @param num_unique_genes
+#' @param unique_gene_names
+#' @param gene_vec
+#' @param bin_mat
+#' @param pred_impact
+#' @param impact
+#'
+#' @return list of gene_mats aggregated by impact
+#' @noRd
+get_gene_mat_by_impact <- function(num_unique_genes, unique_gene_names, gene_vec, bin_mat, pred_impact, impact) {
+  gene_mat <- matrix(NA, nrow = num_unique_genes, ncol = ncol(bin_mat))
+  row.names(gene_mat) <- paste(unique_gene_names, impact, sep = '|')
+  colnames(gene_mat) <- colnames(bin_mat)
+
+  if (impact == 'ALL') {
+    impact <- c('MODERATE', 'MODIFIER', 'HIGH', 'LOW')
+  }
+
+  for (i in 1:num_unique_genes) {
+    current_gene <- unique_gene_names[i]
+    temp_snp_mat <- bin_mat[gene_vec == current_gene & pred_impact %in% impact, , drop = FALSE]
+    gene_mat[i, ] <- colSums(temp_snp_mat)
+    gene_mat[i, ] <-  as.numeric(as.logical(gene_mat[i, ]))
+  }
+
+  gene_mat = gene_mat[rowSums(gene_mat) > 0, ,drop = FALSE]
+  return(gene_mat)
+}
+
+collapse_snps_into_genes_by_impact <- function(bin_mat, gene_vec, predicted_impact){
+  check_is_this_class(gene_vec, "character")
+  check_if_binary_matrix(bin_mat)
+
+  if (length(gene_vec) != nrow(bin_mat)) {
+    stop("gene_vec should have same length as rows of bin_mat")
+  }
+
+  unique_gene_names <- unique(gene_vec)
+  num_unique_genes <- length(unique_gene_names)
+
+  gene_mat_modifier <- get_gene_mat_by_impact(num_unique_genes, unique_gene_names,
+                                              gene_vec, bin_mat, predicted_impact,
+                                              'MODIFIER')
+  gene_mat_high <- get_gene_mat_by_impact(num_unique_genes, unique_gene_names,
+                                          gene_vec, bin_mat, predicted_impact,
+                                          'HIGH')
+  gene_mat_moderate <- get_gene_mat_by_impact(num_unique_genes, unique_gene_names,
+                                              gene_vec, bin_mat, predicted_impact,
+                                              'MODERATE')
+  gene_mat_low <- get_gene_mat_by_impact(num_unique_genes, unique_gene_names,
+                                         gene_vec, bin_mat, predicted_impact, 'LOW')
+
+  gene_mat_all <- get_gene_mat_by_impact(num_unique_genes, unique_gene_names,
+                                         gene_vec, bin_mat, predicted_impact,
+                                        'ALL')
+
+  return(list(gene_mat_all = gene_mat_all,
+              gene_mat_modifier = gene_mat_modifier,
+              gene_mat_high = gene_mat_high,
+              gene_mat_moderate = gene_mat_moderate,
+              gene_mat_low = gene_mat_low))
+
+}
+
+
