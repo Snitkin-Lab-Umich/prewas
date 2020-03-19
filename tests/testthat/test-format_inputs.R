93e018e1356ba0626552b0035e2568da86e2474e
Katie Saund <katiephd@umich.edu>
2020-03-19 11:18:21 -0400

    Update unit test to reflect additional outputs

--- tests/testthat/test-format_inputs.R
+++ tests/testthat/test-format_inputs.R
@@ -1,6 +1,7 @@
 # format_inputs ---------------------------------------------------------------#
 # format_inputs ---------------------------------------------------------------#
 test_that("format_inputs gives expected results prewas test data", {
+  # Set up
   vcf <- prewas::vcf
   tree <- prewas::tree
   outgroup <- prewas::outgroup
@@ -9,7 +10,9 @@ test_that("format_inputs gives expected results prewas test data", {
                            tree = tree,
                            outgroup = outgroup,
                            gff = gff)
-  expect_equal(length(results), 4)
+
+  # Tests
+  expect_equal(length(results), 7)
   expect_true(methods::is(results$dna, "matrix"))
   expect_true(methods::is(results$tree, "phylo"))
   expect_true(methods::is(results$outgroup, "character"))
