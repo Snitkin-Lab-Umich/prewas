99bb3e5d86f3972826c4e3ac0734977e3ae11b93
sthiede <thiede@umich.edu>
2020-03-20 16:56:08 -0400

    added unit tests for dup_snps_in_overlapping_genes_snpeff

--- tests/testthat/test-dup_snps_in_overlapping_genes.R
+++ tests/testthat/test-dup_snps_in_overlapping_genes.R
@@ -19,3 +19,34 @@ test_that("dup_snps_in_overlapping_genes() gives error when given invalid inputs
   expect_error(dup_snps_in_overlapping_genes(matrix(0, 10, 10), "foo"))
   expect_error(dup_snps_in_overlapping_genes(matrix(0, 10, 10), matrix(0, 10, 10)))
 })
+
+# dup_snps_in_overlapping_genes_snpeff ----------------------------------------#
+test_that("dup_snps_in_overlapping_genes_snpeff() works as expected when given valid inputs", {
+
+  temp_bin_mat <- matrix(c(0, 0, 1), nrow = 7, ncol = 3)
+  colnames(temp_bin_mat) <- c("t1", "t2", "t3")
+  row.names(temp_bin_mat) <- c(25:31)
+
+  temp_predicted_impact <- c('LOW', 'LOW|HIGH', 'MODERATE', 'HIGH|HIGH', 'MODIFIER', 'LOW', 'LOW|HIGH')
+  temp_gene <- c('gene1', 'gene2|gene3', 'gene4', 'gene5|gene6', 'gene7', 'gene8', 'gene9|gene10')
+
+  temp_dup_mat <- dup_snps_in_overlapping_genes_snpeff(temp_bin_mat, temp_predicted_impact, temp_gene)
+
+  expect_equal(nrow(temp_dup_mat$bin_mat_dup), 10)
+  expect_equal(length(temp_dup_mat$predicted_impact_split), 10)
+  expect_equal(ncol(temp_dup_mat$bin_mat_dup), 3)
+})
+
+test_that("dup_snps_in_overlapping_genes() gives error when given invalid inputs", {
+  temp_bin_mat <- matrix(c(0, 0, 1), nrow = 7, ncol = 3)
+  colnames(temp_bin_mat) <- c("t1", "t2", "t3")
+  row.names(temp_bin_mat) <- c(25:31)
+
+  temp_predicted_impact <- c('LOW', 'LOW|HIGH', 'MODERATE', 'HIGH|HIGH', 'MODIFIER', 'LOW', 'LOW|HIGH')
+  temp_gene <- c('gene1', 'gene2|gene3', 'gene4', 'gene5|gene6', 'gene7', 'gene8', 'gene9|gene10')
+
+  temp_dup_mat <- dup_snps_in_overlapping_genes_snpeff(temp_bin_mat, temp_predicted_impact, temp_gene)
+
+  expect_error(dup_snps_in_overlapping_genes_snpeff("foo", "foo", "foo"))
+  expect_error(dup_snps_in_overlapping_genes_snpeff(matrix(0, 10, 10), "foo", "foo"))
+})
