62233b70d8a45490dc97f3f51c2d381c933874d4
Katie Saund <katiephd@umich.edu>
2020-03-20 11:59:12 -0400

    Update split_multi_to_biallelic_snps unit tests given snpeff features

--- tests/testthat/test-split_multiallelic_snps.R
+++ tests/testthat/test-split_multiallelic_snps.R
@@ -23,10 +23,13 @@ test_that("split_multi_to_biallelic_snps() gives expected output when input with
                                               o_ref = ref,
                                               snpeff = NULL,
                                               o_alt = alt)
-  expect_equal(length(test_split), 3)
+  expect_equal(length(test_split), 6)
   expect_true(methods::is(test_split$mat_split, "matrix"))
   expect_true(methods::is(test_split$ar_results_split, "data.frame"))
   expect_true(methods::is(test_split$split_rows_flag, "integer"))
+  expect_true(methods::is(test_split$o_ref_split, "character"))
+  expect_true(methods::is(test_split$o_alt_split, "character"))
+  expect_true(methods::is(test_split$snpeff_split, "NULL"))
   expect_equal(length(test_split$split_rows_flag), nrow(test_split$mat_split))
   expect_equal(test_mat, test_split$mat_split)
 })
@@ -49,20 +52,32 @@ test_that("split_multi_to_biallelic_snps() gives expected output when input with
   test_ar <- as.data.frame(test_ar)
   test_ar$probability <- as.numeric(as.character(test_ar$probability))
 
-  test_split <- split_multi_to_biallelic_snps(test_mat, test_ar)
-  expect_equal(length(test_split), 3)
+  ref <- c("G", "G", "A", "G")
+  alt <- c("C,T,A", "T", "G", "C")
+  test_split <- split_multi_to_biallelic_snps(mat = test_mat,
+                                              ar_results = test_ar,
+                                              o_ref = ref,
+                                              snpeff = NULL,
+                                              o_alt = alt)
+  expect_equal(length(test_split), 6)
   expect_true(methods::is(test_split$mat_split, "matrix"))
   expect_true(methods::is(test_split$ar_results_split, "data.frame"))
   expect_true(methods::is(test_split$split_rows_flag, "integer"))
+  expect_true(methods::is(test_split$o_ref_split, "character"))
+  expect_true(methods::is(test_split$o_alt_split, "character"))
+  expect_true(methods::is(test_split$snpeff_split, "NULL"))
   expect_equal(length(test_split$split_rows_flag), nrow(test_split$mat_split))
   expect_equal(sum(grepl(1, test_split$split_rows_flag)), 3)
   expect_equal(nrow(test_split$mat_split), nrow(test_mat) + 2)
   expect_equal(test_split$mat_split[1, ], test_split$mat_split[2, ])
   expect_equal(test_split$mat_split[2, ], test_split$mat_split[3, ])
+  expect_equal(length(test_split$o_ref_split), 6)
+  expect_equal(length(test_split$o_alt_split), 6)
+
 })
 
 test_that("split_multi_to_biallelic_snps() gives error given invalid inputs", {
-  expect_error(split_multi_to_biallelic_snps("foo", "bar"))
-  expect_error(split_multi_to_biallelic_snps(as.data.frame(matrix(0, 1, 1)), "bar"))
-  expect_error(split_multi_to_biallelic_snps(matrix(0, 1, 1), "bar"))
+  expect_error(split_multi_to_biallelic_snps("foo", "bar", "foo", "bar", "foo"))
+  expect_error(split_multi_to_biallelic_snps(as.data.frame(matrix(0, 1, 1)), "bar", "foo", "bar", "foo"))
+  expect_error(split_multi_to_biallelic_snps(matrix(0, 1, 1), "bar", "foo", "bar", "foo"))
 })
